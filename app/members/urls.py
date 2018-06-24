ae5d27c67b872f12b41623a73cb19e16a83c665d
nadcdc <nadcdc4@gmail.com>
2018-06-25 03:47:05 +0900

    login view구현

--- app/members/urls.py
+++ app/members/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 
+app_name = 'members'
+
 urlpatterns = [
 
-    path('login/', views.login_view ),
+    path('login/', views.login_view, name='login'),
 
 
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
@@ -15,4 +17,29 @@ def login_view(request):
     # 6. form 작성후에는 POST방식 요청을 보내서 이 뷰에서 request,POST요청이 잘 왔는지 확인
     # 7. 일단은 받은 username ,password값을 HttpResponse에 보여주도록 한다.
 
-    return HttpResponse('login_view')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #받은 username과 password에 해당하는 User가 있는지 인증.
+        user = authenticate(request, username=username, password=password)
+
+        #인증에 성공한 경우
+        if user is not None:
+            #세션값을 만들어 DB예 저장하과 HTTP response의 Cookie에 해당값을 담아보내도록 하는
+            #login()함수를 실행한다.
+            login(request, user)
+            #dlgn post-list로 redirect
+            return redirect('posts:post-list')
+        #인증에 실패한 경우 (username또는 password가 틀린경우)
+        else:
+            #다시 로그인 페이지로 redirect
+            return redirect('members:login')
+
+        #인증에 성공하면 post:post-list로 이동
+        #실패하면 다시 members:login으로 이동.
+
+    #GET 요청일 경우
+    else:
+        return render(request, 'members/login.html')
