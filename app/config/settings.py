50dcfa8c06fce1feb83ac10dbcdc21ccf322cdea
nadcdc <nadcdc4@gmail.com>
2018-06-26 16:11:34 +0900

    데이터베이스postgresql로 변경함

--- app/config/settings.py
+++ app/config/settings.py
@@ -88,9 +88,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+
+    # 'instagram' 이라는 이름을 사용하는 'PostgreSQL'데이터베이스로 변경
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'instagram',
+        'USER':'nasanmaro',
+        'PASSWORD':'zpffhrm2',
+        'HOST':'localhost',
+        'PORT':'5432',
+
+
     }
 }
 
