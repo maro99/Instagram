8ba513ece77b41bbfb61bf27fa36587f5fd00e9e
nadcdc <nadcdc4@gmail.com>
2018-06-27 02:30:11 +0900

    base.html --> content 출력 추가, 글작성 버튼 추가

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -7,6 +7,6 @@ app_name ='posts'
 urlpatterns = [
      path('', views.post_list, name='post-list'),
      path('<int:pk>/', views.post_detail, name='post-detail'),
-     path('create/',views.post_create)
+     path('create/',views.post_create, name='post-create')
 ]
 
