c8bb1b6c776dc52af2f0642dd83940d62540a849
nadcdc <nadcdc4@gmail.com>
2018-06-28 14:20:56 +0900

    강사님과 몇가지 고친후 그것은 주석처리하고 원래 property쓰는 것 살려놓음

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render, redirect, get_object_or_404
 
 #post_list(request)
@@ -82,33 +82,30 @@ def post_create(request):
     return render(request, 'posts/post_create.html',context)
 
 
-# @login_required
-# @require_POST
-# def post_delete(request,pk):
-#
-#     post = get_object_or_404(Post, pk = pk)
-#     if post.author != request.user:
-#         raise PermissionDenied('지울 권한이 없습니다.')
-#     post.delete()
-#
-#     return redirect('posts:post-list')
-
-
-#위 두개의 decorator 없이 구현해보자.
+@login_required
+@require_POST
 def post_delete(request,pk):
 
-    if request.method =='POST':
-        if request.user.is_anonymous==False:
-
-            post = get_object_or_404(Post, pk =pk)
-
-            if post.author != request.user:
-                raise PermissionDenied('지울 권한이 없습니다.')
-            post.delete()
-        else:
-            return redirect('members:login')
+    post = get_object_or_404(Post, pk = pk)
+    if post.author != request.user:
+        raise PermissionDenied('지울 권한이 없습니다.')
+    post.delete()
 
     return redirect('posts:post-list')
+
+
+# #위 두개의 decorator 없이 구현해보자.
+# def post_delete(request,pk):
+#
+#     if request.method =='POST':
+#         return HttpResponseNotAllowed()
+#
+#     if not request.user.is_authenticated:
+#         return redirect('members:login')
+#
+#     #나머지는 같다.
+
+
 
 
 
